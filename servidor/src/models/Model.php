7b2d52c9ef152d4be325b060168ee27853ac3907
Andres Villanueva <andres_ioi@hotmail.com>
2020-06-03 18:34:02 -0500

    Se corrigio el error de la ruta de Model.php

--- servidor/src/models/Model.php
+++ servidor/src/models/Model.php
@@ -1,7 +1,6 @@
 <?php 
 
-require 'C:\Users\Andi-PC\Documents\PHP\blog\servidor\src\lib\Database.php';
-
+require_once __DIR__.'\..\lib\Database.php';
 class Model {
     protected $db;
     public function __construct(){
